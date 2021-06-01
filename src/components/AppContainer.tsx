4591fbdac802f117be1785a0ca1448c9f9a3474f
Maxwell Lee <maxwell.lee@revature.net>
2021-06-01 12:36:55 -0700

    Beginning of my practice using another API

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AppContainer() {
+  return (
+    <div>
+      <header>
+        <p>
+          Welcome to the beginning of my practice using Square's API
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default AppContainer;
